8df483418abf145548330de30ce041843cb6f074
GitHub Action <action@github.com>
2021-03-04 22:01:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/battagliandrea/mvisetup/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/battagliandrea/mvisetup/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.battagliandrea.mvisetup.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.battagliandrea.mvisetup.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/battagliandrea/mvisetup/services/MyProjectService.kt
+++ src/main/kotlin/com/github/battagliandrea/mvisetup/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.battagliandrea.mvisetup.services
 
+import com.github.battagliandrea.mvisetup.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
